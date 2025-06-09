4b2ab06fad8b4ba0c761ab430452864de28482d4
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-09 11:37:09 +0530

    second commit

---
+++ Even.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Even{
+public static void main(String[] args){
+for(int i=0;i<100;i++){
+if(i%2==0){
+System.out.println(i+" "+"even");
+}
+else{
+System.out.println(i+" "+"odd");
+}
+}
+
+}
+}
